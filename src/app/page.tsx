c1421fd1d97b5fa2c060302f05349bc6b4ab816b
Mayla Yaffa <maylayaffa12@gmail.com>
2025-03-16 22:23:37 +0700

    fix: riwayat aktivitas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,10 @@
-import Link from "next/link";
 import ActivityBox from "./_components/activityBox";
 
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
 
   return (
       <main className="flex-row justify-center items-center p-4 bg-[#C7F9CC]">
-      <div className="text-center">
-        <h1 className="text-xl text-bold text-blue-900">Riwayat Aktivitas</h1>
-      </div>
-      <div>
-        <ActivityBox activity="Lari Pagi" date="14 Februari 2025"/>
-        <ActivityBox activity="Lari Pagi" date="14 Februari 2025"/>
-        <ActivityBox activity="Lari Pagi" date="14 Februari 2025"/>
-        <ActivityBox activity="Lari Pagi" date="14 Februari 2025"/>
-      </div>
+      uiiaiuiia
       </main>
   );
 }
