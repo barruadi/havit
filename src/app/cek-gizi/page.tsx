fadb26a4784105f82b4c91051f6f355f7c8a1045
Barru Adi <barru.adi@gmail.com>
2025-03-23 21:04:55 +0700

    feat: ai integration

--- src/app/cek-gizi/page.tsx
+++ src/app/cek-gizi/page.tsx
@@ -1,23 +1,50 @@
 "use client"
 
+import { model } from "../api/gemini/route";
+
 // import components
 import BahanMakanan from "./BahanMakanan"
 import Button from "../_components/button";
 
 import { use, useState } from "react"
+import React from "react"
 
 function CekGizi() {
 
     const [inputGizi, setInputGizi] = useState("");
     const [listBahan, setListBahan] = useState<string[]>([]);
-
-    const handleProcess = (e: FormData) => {
+    
+    type GiziType = {
+        Kalori: number;
+        Protein: number;
+        Lemak: number;
+        Karbohidrat: number;
+        Vitamin: string[];
+    }
+    
+    const [gizi, setGizi] = useState<GiziType>({
+        Kalori: 0,
+        Protein: 0,
+        Lemak: 0,
+        Karbohidrat: 0,
+        Vitamin: []
+    });
+    
+    const handleProcess = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
         console.log(inputGizi);
-
+        
         // fetch AI
-
+        const prompt = `List estimasi gizi makanan dari menu berikut: ${inputGizi}
+        
+        Gizi = {'Kalori': int, 'Protein': int, 'Lemak': int, 'Karbohidrat': int, 'Vitamin': [str]}
+        Return: Gizi[];
+        `;
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+        
         // add to list
-        setListBahan([]);
+        // setListBahan(result);
 
     }
 
@@ -36,14 +63,21 @@ function CekGizi() {
             </div>
             {/* inputnya */}
             <div className="mx-5">
-                <textarea 
-                    value={inputGizi}
-                    onChange={(e) => setInputGizi(e.target.value)}
-                    className="py-3 w-full px-6 rounded-[14px] 
-                bg-[#83CCAB] text-white border border-[#83CCAB]
-                overflow-hidden placeholder:text-gray-300"
-                    placeholder="Masukkan makanan"
-                />
+                <form action="">
+                    <textarea 
+                        value={inputGizi}
+                        onChange={(e) => setInputGizi(e.target.value)}
+                        className="py-3 w-full px-6 rounded-[14px] 
+                        bg-[#83CCAB] text-white border border-[#83CCAB]
+                        overflow-hidden placeholder:text-gray-300"
+                        placeholder="Masukkan makanan"
+                    />
+
+                    <button
+                        onClick={handleProcess}>
+                            Submit
+                    </button>
+                </form>
             </div>
 
             {/* List bahan makanan */}
